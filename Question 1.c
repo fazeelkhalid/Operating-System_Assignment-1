62a92b5f8dd79e3ee5adbf06487d96b4aae7fdcb
Fazeel Khalid <fazeelkhalid9@gmail.com>
2021-03-20 11:24:16 +0500

    updated version

--- Question 1.c
+++ Question 1.c
@@ -1,11 +1,11 @@
 /*
 ---------------------------------------------------
 ---------------------------------------------------
-----This code was written on Dev-C++ 5.11----------
+-------This code was written on Dev-C++ 5.11-------
 ---------------------------------------------------
 ---------------------------------------------------
 */
-
+#include<conio.h>
 #include<stdio.h>
 int stringLen(char *str){ // calculate string length
 	int i;
@@ -15,20 +15,23 @@ int stringLen(char *str){ // calculate string length
 
 
 int searchInFile(FILE *fp, char*str){
-	char fChar; // store characters from file
+	char fChar = ' '; // store characters from file
 	int index = 0;//string index
 	
 	while(fChar != EOF){
-		fChar = getc(fp); // read a character from file
-		if(str[index] == fChar){ // character match
-			
+
+		if(str[index] == fChar){ // character match	
 			index++;
 			if(index == stringLen(str)){
 				return 1; // means string match
 			}
-		}	
+			fChar = getc(fp); 
+		}
 		else{
 			index = 0;
+			if(str[index] != fChar){
+				fChar = getc(fp); // read a character from file
+			}
 		}
 	}
 	return 0; // means string not match
@@ -36,31 +39,31 @@ int searchInFile(FILE *fp, char*str){
 
 int main() {
 
-	FILE *fp = malloc(1*sizeof(fp));// point file pointer
+	FILE *fp;// point file pointer
 	char fName[100] = {0}; // File name
 	char subStr[100];
+	int count = 0; // count how many time s string match
 	
 	//take input file path plus name
 	printf("\nEnter file name with path : ");
-	gets(fName);
+	gets_s(fName,100);
 	
-	fp = fopen(fName , "r"); // open file in read mode
+	fopen_s(&fp,fName , "r"); // open file in read mode
 	
 	if (fp != NULL ) {// check is file open
 		//substring that you wan'na search
 		printf("\nstring that you want to search from file : ");
-		gets(subStr);
+		gets_s(subStr,100);
 		
-		if(searchInFile(fp,subStr)){ // is string found
-			printf("\nSub string present :)");	
+		while(searchInFile(fp,subStr)){ // is string found or untill file not read completely
+			count++;
 		}
-		else{
-			printf("\nSub string not present :(");
-		}
+		printf("\nNumber of occurrences of substring : %d",count);
 	}
 	else {
 		printf("\nFile not found :(");
 	}
-
+	fclose(fp);
+	_getch();
 	return 0;
 }
